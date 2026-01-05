7ed8169ba1944a81edeea9948060be7dfec47c83
CamiloMunoz198 <ingeniero.master94@interrapidisimo.com>
2026-01-05 14:22:01 -0500

    V6.0.2
    Ajuste Sppiner Lista de Usuarios

--- src/app/components/usuariosApp/listado-usuarios/listado-usuarios.component.ts
+++ src/app/components/usuariosApp/listado-usuarios/listado-usuarios.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { UsuariosJsonPlaceHolderService } from '../../../services/usuarios-json-place-holder.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-listado-usuarios',
@@ -7,15 +8,38 @@ import { UsuariosJsonPlaceHolderService } from '../../../services/usuarios-json-
   templateUrl: './listado-usuarios.component.html',
   styleUrls: ['./listado-usuarios.component.css'],
 })
-export class ListadoUsuariosComponent implements OnInit {
+export class ListadoUsuariosComponent implements OnInit, OnDestroy{
   usuariosList: any[] = [];
   constructor(private usuarioSevice: UsuariosJsonPlaceHolderService) {}
 
+  cargando: boolean = true; // Inicia en true para mostrar el loader de inmediato
+  loaderVisible: boolean = true; // Controla la opacidad (CSS)
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  private destroy$ = new Subject<void>(); // Para limpieza de memoria
   ngOnInit() {
     this.usuarioSevice
       .obtenerDatosUsuarioJsonPlaceHolder()
-      .subscribe((dataUsuariosApi) => {
-        this.usuariosList = dataUsuariosApi;
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({        
+         next: (dataUsuariosApi) => {  
+          this.usuariosList = dataUsuariosApi; 
+
+        // 2. Esperamos los 0.3s del CSS para quitarlo del DOM
+        setTimeout(() => {
+            // 1. Iniciamos el desvanecimiento de salida
+        this.loaderVisible = false;
+          this.cargando = false;
+        }, 300);
+        },
+        error: (err) => {
+          console.error("Error cargando clientes", err);
+          this.cargando = false;
+        }
       });
   }
 }
